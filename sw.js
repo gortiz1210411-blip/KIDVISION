ff4cee907c7223e450817967b950d74ea1c76029
gortiz1210411-blip <gortiz1210.411@gmail.com>
2025-08-17 14:39:07 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-const CACHE='kidvision-v121-admin-norm';
+const CACHE='kidvision-v121-admin-norm_v122admfix';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./kidvision_logo.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE&&caches.delete(k)))));self.clients.claim();});
